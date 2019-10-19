2eade52d6bc505a86d7f514ca3efa13a409a89e6
Alexander Moerman <a.moerman@dobcomed.com>
2019-10-19 15:44:02 +0200

    Add solution for exercise 1

--- UnitTestingWorkshop.Tests/Exercise01WordCombinationsFinder/TestsForWordCombinationsFinder.cs
+++ UnitTestingWorkshop.Tests/Exercise01WordCombinationsFinder/TestsForWordCombinationsFinder.cs
@@ -19,31 +19,50 @@ namespace UnitTestingWorkshop.Tests.Exercise01WordCombinationsFinder
             public void ShouldReturnEmptyWhenThereAreNoWords()
             {
                 // Arrange
+                var words = new string[0];
+                var requestedLength = 6;
 
                 // Act
+                var combinations = _finder.FindCombinations(words, requestedLength);
 
                 // Assert
+                combinations.Should().BeEmpty();
             }
 
             [Fact]
             public void ShouldReturnEmptyWhenThereAreNoCombinations()
             {
                 // Arrange
+                var words = new string[] { "pizza", "hamburgers", "fries" };
+                var requestedLength = 6;
 
                 // Act
+                var combinations = _finder.FindCombinations(words, requestedLength);
 
                 // Assert
+                combinations.Should().BeEmpty();
             }
-
 
             [Fact]
             public void ShouldReturnTwoCombinationsWhenThereAreTwoCombinations()
             {
                 // Arrange
+                var words = new string[] { 
+                    "al", "bums", "albums",
+                    "tail", "or", "tailor"
+                };
+                var requestedLength = 6;
 
                 // Act
+                var combinations = _finder.FindCombinations(words, requestedLength);
 
                 // Assert
+                var expectedCombinations = new[]
+                {
+                    new WordCombination { Word1 = "al", Word2 = "bums", Combination = "albums" },
+                    new WordCombination { Word1 = "tail", Word2 = "or", Combination = "tailor" },
+                };
+                combinations.Should().BeEquivalentTo(expectedCombinations);
             }
         }
     }
