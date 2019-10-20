8ed1103aad1844ea7177d6fa6a8d7474d2672140
Alexander Moerman <a.moerman@dobcomed.com>
2019-10-20 10:29:30 +0200

    Adjust namespaces

--- UnitTestingWorkshop.Core/Exercise02Authentication/UserNamePasswordValidator.cs
+++ UnitTestingWorkshop.Core/Exercise02Authentication/UserNamePasswordValidator.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace UnitTestingWorkshop.Core.Exercise02SMS
+namespace UnitTestingWorkshop.Core.Exercise02Authentication
 {
     public interface IUserNamePasswordValidator
     {
