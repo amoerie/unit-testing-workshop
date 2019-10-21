4bca17448fbd3e7f22cb7e27e7dba5e2650bb16f
Alexander Moerman <a.moerman@dobcomed.com>
2019-10-21 17:02:25 +0200

    Add solution for exercise 4

--- UnitTestingWorkshop.Core/Exercise04Dinosaurs/Dinosaur.test.js
+++ UnitTestingWorkshop.Core/Exercise04Dinosaurs/Dinosaur.test.js
@@ -6,34 +6,46 @@ describe("Dinosaur", function () {
 
         test("should be false when the dinosaur is smaller than 1 meter", function () {
             // Arrange
+            const dinosaur = new Dinosaur("Microraptor", 0.6, [ "claw", "bite" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
             // Assert 
+            expect(isDangerous).toBeFalsy();
         });
 
         test("should be false when the dinosaur cannot claw or bite", function () {
             // Arrange
+            const dinosaur = new Dinosaur("Europasaurus", 6.2, [ "", "" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
-            // Assert    
+            // Assert 
+            expect(isDangerous).toBeFalsy();
         });
 
         test("should be true when the dinosaur is larger than 1m and can claw", function () {
             // Arrange
+            const dinosaur = new Dinosaur("T-Rex", 6.2, [ "claw" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
-            // Assert    
+            // Assert 
+            expect(isDangerous).toBeTruthy(); 
         });
 
         test("should be true when the dinosaur is larger than 1m and can bite", function () {
             // Arrange
+            const dinosaur = new Dinosaur("T-Rex", 6.2, [ "bite" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
-            // Assert    
+            // Assert 
+            expect(isDangerous).toBeTruthy(); 
         });
     });
 
--- UnitTestingWorkshop.Core/Exercise04Dinosaurs/Dinosaur2.test.ts
+++ UnitTestingWorkshop.Core/Exercise04Dinosaurs/Dinosaur2.test.ts
@@ -4,36 +4,48 @@ describe("Dinosaur2", () => {
 
     describe("isDangerous", () => {
 
-        test("should be false when the dinosaur is smaller than 1 meter", () => {
+        test("should be false when the dinosaur is smaller than 1 meter", () =>  {
             // Arrange
+            const dinosaur = new Dinosaur2("Microraptor", 0.6, [ "claw", "bite" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
             // Assert 
+            expect(isDangerous).toBeFalsy();
         });
 
-        test("should be false when the dinosaur cannot claw or bite", () => {
+        test("should be false when the dinosaur cannot claw or bite", () =>  {
             // Arrange
+            const dinosaur = new Dinosaur2("Europasaurus", 6.2, [ "", "" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
-            // Assert    
+            // Assert 
+            expect(isDangerous).toBeFalsy();
         });
 
-        test("should be true when the dinosaur is larger than 1m and can claw", () => {
+        test("should be true when the dinosaur is larger than 1m and can claw", () =>  {
             // Arrange
+            const dinosaur = new Dinosaur2("T-Rex", 6.2, [ "claw" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
-            // Assert    
+            // Assert 
+            expect(isDangerous).toBeTruthy();
         });
 
-        test("should be true when the dinosaur is larger than 1m and can bite", () => {
+        test("should be true when the dinosaur is larger than 1m and can bite", () =>  {
             // Arrange
+            const dinosaur = new Dinosaur2("T-Rex", 6.2, [ "bite" ]);
 
             // Act
+            const isDangerous = dinosaur.isDangerous();
 
-            // Assert    
+            // Assert 
+            expect(isDangerous).toBeTruthy();
         });
     });
 
